f3e2285c508a8b304fb521c908ee8a67d6f7999b
Quoi Lam <quoi.l@servergy.com>
2015-03-23 13:37:01 -0500

    Update ccform loading to settings.php as Ben's request

--- js/billing.js
+++ js/billing.js
@@ -1,5 +1,7 @@
 $(document).ready(function()
 {
+	var msg = eleid = '';
+
 	$("#tierid").change(function()
 	{
 		var state = $('option:selected', this).attr('state');
@@ -29,24 +31,87 @@ $(document).ready(function()
 	});
 
 	$("#storagebutton").click( function(){
-		// Serialize the data
-		var post = $( "#storageform" ).serialize();
-		$('#storagechanged').hide();
-		$('#storageerror').hide();
-		// Ajax foo
-		$.post(OC.filePath( 'registration', 'ajax', 'changestorage.php' ), post, function(data){
-			if( data.status === "success" ){
-				$('#storagechanged').show();
-				$('#storageform').hide();
-			} else{
-				if (typeof(data.data) !== "undefined") {
-					$('#storageerror').html(data.data.message);
-				} else {
-					$('#storageerror').html(t('Unable to change storage'));
+		ccValidate();
+		if (!msg) {
+			// Serialize the data
+			var post = $( "#storageform" ).serialize();
+			$('#storagechanged').hide();
+			$('#storageerror').hide();
+			// Ajax foo
+			$.post(OC.filePath( 'registration', 'ajax', 'changestorage.php' ), post, function(data){
+				if( data.status === "success" ){
+					$('#storagechanged').show();
+					$('#storageform').hide();
+				} else{
+					if (typeof(data.data) !== "undefined") {
+						$('#storageerror').html(data.data.message);
+					} else {
+						$('#storageerror').html(t('Unable to change storage'));
+					}
+					$('#storageerror').show();
 				}
-				$('#storageerror').show();
-			}
-		});
+			});
+		} else {
+			$( '#formMsg' ).text(msg);
+			$( '#formMsgContainer' ).show().fadeOut(4000, function() {
+				$(eleid).focus();
+			});
+		}
 		return false;
 	});
+
+	// validattion on fields those are NOT handled by formValidation:
+	$( "#regist" ).submit(function( event ) {
+		if($("#password").val() !== $("#password-clone").val()) {
+			msg = 'Passwords do not match';
+			eleid = "#password";
+		} else if ($("#tierid option:selected").index()) {
+			ccValidate();
+		}
+
+		if (msg) {
+			$( '#formMsg' ).text(msg);
+			$( '#formMsgContainer' ).show().fadeOut(4000, function() {
+				$(eleid).focus();
+			});
+			event.preventDefault(); // Prevent form from submit ELSE return;
+			return false;
+		} else {
+
+			return true;
+		}
+
+	});
+
+	function ccValidate() {
+		msg = eleid = '';
+		if (!$("#firstname").val()) {
+			msg = 'Firstname is required';
+			eleid = "#firstname";
+		} else if(!$("#lastname").val()) {
+			msg = 'Lastname is required';
+			eleid = "#lastname";
+		} else if(!$("#address").val()) {
+			msg = 'Address is required';
+			eleid = "#address";
+		} else if(!$("#city").val()) {
+			msg = 'City is required';
+			eleid = "#city";
+		} else if(!$("#state").val()) {
+			msg = 'State is required';
+			eleid = "#state";
+		} else if(!$("#country option:selected").index()) {
+			msg = 'Country is required';
+			eleid = "#country";
+		} else if(!$("#zip").val()) {
+			msg = 'Zip is required';
+			eleid = "#zip";
+		} else if(!$("#cc_cardnum").val()||!/^\d+$/.test($("#cc_cardnum").val())) {
+			msg = 'Credit Card Number is required (Digits ONLY)';
+			eleid = "#cc_cardnum";
+		} else if(!$("#cc_ccv").val()||!/^\d+$/.test($("#cc_ccv").val())) {
+			msg = 'Security Card Code is required (Digits ONLY)';
+			eleid = "#cc_ccv";
+		}
+	}
 });
--- templates/settings.php
+++ templates/settings.php
@@ -1,14 +1,10 @@
-<?php
-$config = \OC::$server->getConfig();
-$uid = \OC_User::getUser();
-$quota = $config->getUserValue($uid, 'files', 'quota');
-?>
+<?php OCP\Util::addStyle('core', 'guest'); ?>
 
 <div id="quota" class="section">
         <div style="width:<?php p($_['usage_relative']);?>%;">
                 <p id="quotatext">
 <?php
-if (!empty($quota)) {
+if (!empty($_['quota'])) {
         print_unescaped($l->t('You have used <strong>%s</strong> of the available <strong>%s</strong>',
                 array($_['usage'], $_['total_space'])));
 } else {
@@ -21,94 +17,28 @@ if (!empty($quota)) {
 </div>
 
 <?php
-$tierid = $config->getUserValue($uid, 'registration', 'tierid');
-if (empty($tierid) or $tierid < 2) {
-	$stmt = \OC_DB::prepare('SELECT * FROM `*PREFIX*tier_table` WHERE tierid=2');
-	$result = $stmt->execute();
-	if (!$result) {
-		throw new \Exception('Invalid service level');
-		return false;
-	}
-
-	$tier = $result->fetchRow();
+if ($_['tierid'] < 2) {
 ?>
+
 <div class="section">
-       <h2><?php p($l->t('Upgrade to ').$tier['description']); ?></h2><br/>
-       <div id="storagechanged"><?php echo $l->t('Your storage was upgraded');?></div>
-        <div id="storageerror"><?php echo $l->t('Unable to upgrade your storage');?></div>
+	<h2><?php p($l->t('Upgrade to ').$_['tier']['description']); ?></h2><br/>
+	<div id="storagechanged"><?php echo $l->t('Your storage was upgraded');?></div>
+	<div id="storageerror"><?php echo $l->t('Unable to upgrade your storage');?></div>
+	<form id="storageform">
+	<fieldset>
 
-<form id="storageform">
-       <p>
-       <label for="firstname"><?php p($l->t('First and Last Name on Card'));?></label><br>
-       <input class="inlineblock" type="text" name="firstname" id="firstname" autocomplete="cc-given-name" value="<?php echo $_['entered_data']['firstname']; ?>" />
-       <input class="inlineblock" type="text" name="lastname" id="lastname" autocomplete="cc-family-name" value="<?php echo $_['entered_data']['lastname']; ?>" />
-       </p><br/>
+		<?php print_unescaped($_['ccform']); ?>
 
-       <p>
-       <label for="country"><?php p($l->t('Country'));?></label><br>
-       <select name="country" id="country">
-       <option value="">-- Select one --</option>
-<?php
-       $stmt = OC_DB::prepare('SELECT * FROM `*PREFIX*countries` WHERE code!=\'\' ORDER BY seq ASC');
-       $result = $stmt->execute(array());
-       while($row = $result->fetchRow()) {
-               print "\t\t<option value=\"". $row['code'] ."\"";
+		<hr/>
+		<div id="formMsgContainer" class="errors" style="display:none;">
+			<p id="formMsg"></p>
+		</div>
 
-               if ($row['code'] == $_['entered_data']['country'])
-                       print " selected";
-
-               print ">". $row['name'] ."</option>\n";
-       }
-?>
-       </select>
-       </p><br/>
-
-       <p>
-       <label for="zip"><?php p($l->t('Zip Code'));?></label><br>
-       <input type="text" name="zip" id="zip" autocomplete="postal-code" inputmode="numeric" value="<?php echo $_['entered_data']['zip']; ?>" />
-       </p><br/>
-
-       <p>
-       <label for="cc_cardnum"><?php p($l->t('Credit Card Number'));?></label><br>
-       <input type="text" name="cc_cardnum" id="cc_cardnum" autocomplete="cc-number" inputmode="numeric" value="<?php echo $_['entered_data']['cc_cardnum']; ?>" />
-       </p><br/>
-
-       <p>
-       <label for="cc_expmonth"><?php p($l->t('Card Expiration'));?></label><br>
-       <select name="cc_expmonth" id="cc_expmonth" autocomplete="cc-exp-month">
-               <option value="1">1 - <?php print_unescaped($l->t( 'January' )); ?></option>
-               <option value="2">2 - <?php print_unescaped($l->t( 'February' )); ?></option>
-               <option value="3">3 - <?php print_unescaped($l->t( 'March' )); ?></option>
-               <option value="4">4 - <?php print_unescaped($l->t( 'April' )); ?></option>
-               <option value="5">5 - <?php print_unescaped($l->t( 'May' )); ?></option>
-               <option value="6">6 - <?php print_unescaped($l->t( 'June' )); ?></option>
-               <option value="7">7 - <?php print_unescaped($l->t( 'July' )); ?></option>
-               <option value="8">8 - <?php print_unescaped($l->t( 'August' )); ?></option>
-               <option value="9">9 - <?php print_unescaped($l->t( 'September' )); ?></option>
-               <option value="10">10 - <?php print_unescaped($l->t( 'October' )); ?></option>
-               <option value="11">11 - <?php print_unescaped($l->t( 'November' )); ?></option>
-               <option value="12">12 - <?php print_unescaped($l->t( 'December' )); ?></option>
-       </select>
-       <select name="cc_expyear" id="cc_expyear" autocomplete="cc-exp-year">
-<?php
-       // 10 years, starting from current year
-       $baseyear = date("Y");
-       for ($i = 0; $i < 10; $i++) {
-               $year = $baseyear + $i;
-               print "\t\t<option value='$year'>$year</option>\n";
-       }
-?>
-       </select>
-       </p><br/>
-
-       <p>
-       <label for="cc_ccv"><?php p($l->t('Card Security Code (ccv)'));?></label><br>
-       <input type="text" name="cc_ccv" id="cc_ccv" autocomplete="cc-csc" value="<?php echo $_['entered_data']['cc_ccv']; ?>" />
-       </p><br/>
-
-       <input id="storagebutton" type="submit" value="<?php echo $l->t('Upgrade Storage');?>" />
+		<input id="storagebutton" type="submit" value="<?php echo $l->t('Upgrade Storage');?>" />
+	</fieldset>
 </form>
 </div>
+
 <?php
 } else {
 ?>
@@ -118,6 +48,8 @@ if (empty($tierid) or $tierid < 2) {
 <?php
 }
 ?>
+
+<?php OC_Util::addScript("registration", "billing");?>
 
 <div class="section">
         <h2><?php p($l->t('Version'));?></h2>
