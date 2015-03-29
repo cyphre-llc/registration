24b0d9704cc9e13e5eb6b49e474c7eeac57d9b09
Quoi Lam <quoi.l@servergy.com>
2015-03-29 11:52:50 -0400

    SVY: Implement full billing fields
    Signed-off-by: Quoi Lam <quoi.l@servergy.com>
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- settings.php
+++ settings.php
@@ -1,11 +1,55 @@
 <?php
+// Read user's settings from database to pass into template:
+$config = \OC::$server->getConfig();
+$uid = \OC_User::getUser();
+$quota = $config->getUserValue($uid, 'files', 'quota');
 
+$tierid = $config->getUserValue($uid, 'registration', 'tierid', 1);
+if ($tierid < 2) {
+	$stmt = \OC_DB::prepare('SELECT * FROM `*PREFIX*tier_table` WHERE tierid=2');
+	$result = $stmt->execute();
+	if (!$result) {
+		throw new \Exception('Invalid service level');
+		return false;
+	}
+	$tier = $result->fetchRow();
+}
+
+// If entered data:
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$entered = $_POST;
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+	$entered = $_GET;
+} else {
+	$entered = array();
+}
+
+// Initialized form's params:
+$entered['firstname'] = array_key_exists('firstname',$entered) ? $entered['firstname'] : '';
+$entered['lastname'] = array_key_exists('lastname',$entered) ? $entered['lastname'] : '';
+$entered['country'] = array_key_exists('country',$entered) ? $entered['country'] : 'US';
+$entered['zip'] = array_key_exists('zip',$entered) ? $entered['zip'] : '';
+$entered['address'] = array_key_exists('address',$entered) ? $entered['address'] : '';
+$entered['address1'] = array_key_exists('address1',$entered) ? $entered['address1'] : '';
+$entered['city'] = array_key_exists('city',$entered) ? $entered['city'] : '';
+$entered['state'] = array_key_exists('state',$entered) ? $entered['state'] : '';
+
+// Fetch ccform:
+$ccform = new OCP\Template('registration', 'ccform');
+$ccform->assign('entered_data', $entered);
+
+// Fetch main setting form:
 $tmpl = new OCP\Template('registration', 'settings');
-
 $storageInfo=OC_Helper::getStorageInfo('/');
 
+// Assigned form's params:
 $tmpl->assign('usage', OC_Helper::humanFileSize($storageInfo['used']));
 $tmpl->assign('total_space', OC_Helper::humanFileSize($storageInfo['total']));
 $tmpl->assign('usage_relative', $storageInfo['relative']);
+
+$tmpl->assign('quota', $quota);
+$tmpl->assign('tierid', $tierid);
+$tmpl->assign('tier', $tier);
+$tmpl->assign('ccform', $ccform->fetchPage());
 
 return $tmpl->fetchPage();
---
+++ templates/ccform.php
@@ -0,0 +1,94 @@
+<br/>
+<p style="text-align:center;"><label><b><?php print_unescaped($l->t('Enter billing information')); ?></b></label></p>
+<hr/>
+<p class="infield">
+	<label for="firstname"><?php print_unescaped($l->t( 'Name on card' )); ?></label><br>
+	<input type="text" name="firstname" id="firstname" autocomplete="cc-given-name" value="<?php echo $_['entered_data']['firstname']; ?>" placeholder="<?php print_unescaped($l->t( 'First Name' )); ?>" />
+	<input type="text" name="lastname" id="lastname" autocomplete="cc-family-name" value="<?php echo $_['entered_data']['lastname']; ?>" placeholder="<?php print_unescaped($l->t( 'Last Name' )); ?>" />
+</p>
+
+<hr/>
+
+<p class="infield">
+	<label for="address"><?php print_unescaped($l->t( 'Street Address' )); ?></label><br>
+	<input type="text" name="address" id="address" autocomplete="street-address" maxlength="50" value="<?php echo $_['entered_data']['address']; ?>" placeholder="<?php print_unescaped($l->t( 'Address Line 1' )); ?>" />
+</p>
+
+<p class="infield">
+	<input type="text" name="address1" id="address1" autocomplete="street-address1" maxlength="50" value="<?php echo $_['entered_data']['address1']; ?>" placeholder="<?php print_unescaped($l->t( 'Address Line 2' )); ?>" />
+</p>
+
+<hr/>
+
+<p class="infield">
+	<input type="text" name="city" id="city" autocomplete="city-address" maxlength="50" value="<?php echo $_['entered_data']['city']; ?>" placeholder="<?php print_unescaped($l->t( 'City or Locality' )); ?>" />
+</p>
+
+<p class="infield">
+	<input type="text" name="state" id="state" autocomplete="state-address" maxlength="2" value="<?php echo $_['entered_data']['state']; ?>" placeholder="<?php print_unescaped($l->t( 'State or Province' )); ?>" />
+</p>
+
+<p class="infield">
+	<div>
+	<select name="country" id="country">
+	<option value="">-- Select Country --</option>
+	<?php
+		$stmt = OC_DB::prepare('SELECT * FROM `*PREFIX*countries` WHERE code!=\'\' ORDER BY seq ASC');
+		$result = $stmt->execute(array());
+		while($row = $result->fetchRow()) {
+			print "\t\t<option value=\"". $row['code'] ."\"";
+
+			if ($row['code'] == $_['entered_data']['country'])
+				print " selected";
+
+			print ">". $row['name'] ."</option>\n";
+		}
+	?>
+	</select>
+	</div>
+</p>
+
+<p class="infield">
+	<input type="text" name="zip" id="zip" autocomplete="postal-code" inputmode="numeric" value="<?php echo $_['entered_data']['zip']; ?>" placeholder="<?php print_unescaped($l->t( 'Zip Code' )); ?>" />
+</p>
+
+<hr/>
+
+<p class="infield">
+	<label for="cc_cardnum"><?php print_unescaped($l->t( 'Credit Card' )); ?></label><br>
+	<input type="text" name="cc_cardnum" id="cc_cardnum" autocomplete="cc-number" inputmode="numeric" value="" placeholder="<?php print_unescaped($l->t( 'Credit Card Number' )); ?>" />
+</p>
+<p class="infield">
+	<label for="cc_expmonth"><?php print_unescaped($l->t( 'Expiration Date' )); ?></label><br>
+	<div>
+	<select name="cc_expmonth" id="cc_expmonth" autocomplete="cc-exp-month">
+		<option value="1">1 - <?php print_unescaped($l->t( 'January' )); ?></option>
+		<option value="2">2 - <?php print_unescaped($l->t( 'February' )); ?></option>
+		<option value="3">3 - <?php print_unescaped($l->t( 'March' )); ?></option>
+		<option value="4">4 - <?php print_unescaped($l->t( 'April' )); ?></option>
+		<option value="5">5 - <?php print_unescaped($l->t( 'May' )); ?></option>
+		<option value="6">6 - <?php print_unescaped($l->t( 'June' )); ?></option>
+		<option value="7">7 - <?php print_unescaped($l->t( 'July' )); ?></option>
+		<option value="8">8 - <?php print_unescaped($l->t( 'August' )); ?></option>
+		<option value="9">9 - <?php print_unescaped($l->t( 'September' )); ?></option>
+		<option value="10">10 - <?php print_unescaped($l->t( 'October' )); ?></option>
+		<option value="11">11 - <?php print_unescaped($l->t( 'November' )); ?></option>
+		<option value="12">12 - <?php print_unescaped($l->t( 'December' )); ?></option>
+	</select>
+
+	<select name="cc_expyear" id="cc_expyear" autocomplete="cc-exp-year">
+	<?php
+	// 10 years, starting from current year
+		$baseyear = date("Y");
+		for ($i = 0; $i < 10; $i++) {
+			$year = $baseyear + $i;
+			print "\t\t<option value='$year'>$year</option>\n";
+		}
+	?>
+	</select>
+	</div>
+</p>
+
+<p class="infield">
+	<input type="text" name="cc_ccv" id="cc_ccv" autocomplete="cc-csc" value="" placeholder="<?php print_unescaped($l->t( 'Card Security Code' )); ?>" />
+</p>
